047a29cf0085f2963c5a3bffb3eb31a02ac00b81
mohammad606 <mohammad123alhalabi123@gmail.com>
2026-01-17 06:04:37 +0200

    create plans and sub and noti table

---
+++ database/migrations/2026_01_17_040316_create_subscriptions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotel_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('plan_id')->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['active', 'expired', 'cancelled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
