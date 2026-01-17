22dddcd3ffa64888b724787f6eae27b9e2c4d302
mohammad606 <mohammad123alhalabi123@gmail.com>
2026-01-17 05:54:28 +0200

    create hotel and rooms image table and rooms table

---
+++ database/migrations/2026_01_17_035252_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotel_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('base_price', 8, 2);
+            $table->integer('max_guests');
+            $table->integer('total_rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
